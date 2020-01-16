904293b2a9138fba835454dfdce223b59680824e
forderation <kharisma.muzaki@gmail.com>
2020-01-16 10:48:51 +0700

    contacted flow with upload photo OK)

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ data_recap = {
 
 
 def date_handler(update, context):
-    update.message.reply_text(
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
         text="Masukkan tanggal visit: ",
         reply_markup=telegram_utils.create_calendar()
     )
@@ -49,38 +50,46 @@ def date_callback(update, context):
     selected, date = telegram_utils.process_calendar_selection(update, context)
     if selected:
         date = date.strftime("%Y-%m-%d")
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Tanggal visit : %s" % date,
-            reply_markup=ReplyKeyboardRemove()
-        )
+        query = update.callback_query
+        query.edit_message_text(text="Tanggal visit : %s" % date)
         data_recap["date"] = date
         state_handler(update, context)
         return STATE_VS
 
 
 def start(update, context):
-    update.message.reply_text(
-        "---- Hallo selamat datang di bot visit-ctb telkom ----"
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="---- Hallo selamat datang di bot visit-ctb telkom ----",
+        reply_markup=ReplyKeyboardRemove()
     )
     cust_number(update, context)
     return INPUT_CUST
 
 
 def cust_number(update, context):
-    update.message.reply_text("Masukkan terlebih dahulu Nomor Internet pelanggan: ")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Masukkan terlebih dahulu Nomor Internet pelanggan: "
+    )
 
 
 def cust_callback(update, context):
     cust_num = update.message.text
-    update.message.reply_text(text="Nomor Internet pelanggan : {}".format(cust_num))
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Nomor Internet pelanggan : {}".format(cust_num)
+    )
     data_recap["nip"] = cust_num
     date_handler(update, context)
     return DATE_VS
 
 
 def cancel_callback(update, context):
-    update.message.reply_text(text="Sesi input visit CTB Berakhir, Tekan /start untuk mulai kembali")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sesi input visit CTB Berakhir, Tekan /start untuk mulai kembali"
+    )
     return ConversationHandler.END
 
 
@@ -89,16 +98,23 @@ def photo_handler(update, context):
 
 
 def photo_callback(update, context):
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+
+    )
     pass
 
 
 def state_handler(update, context):
     msg = "Silahkan pilih terlebih dahulu status hasil visit :"
-    keyboard = [[InlineKeyboardButton("contacted", callback_data="contacted"),
-                 InlineKeyboardButton("not contacted", callback_data="not_contacted")]]
+    keyboard = [[InlineKeyboardButton("not contacted", callback_data="not_contacted"),
+                 InlineKeyboardButton("contacted", callback_data="contacted")]]
     keyboard_type_visit = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(
-        chat_id=update.callback_query.from_user.id,
+        chat_id=update.effective_chat.id,
         text=msg,
         reply_markup=keyboard_type_visit
     )
@@ -141,7 +157,7 @@ def contact_handler(update, context):
     ]
     keyboard_type_visit = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(
-        chat_id=update.callback_query.from_user.id,
+        chat_id=update.effective_chat.id,
         text=msg,
         reply_markup=keyboard_type_visit
     )
@@ -149,26 +165,22 @@ def contact_handler(update, context):
 
 def fallback_handler(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
-    update.message.reply_text(text="Error: Perintah tidak dikenali")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Error: Perintah tidak dikenali : %s" % context.error
+    )
 
 
 def confirm_handler(update, context):
-    recap_data = """
-    ----- rekap data input -----
-    nomor internet pelanggan : {} 
-    tanggal kunjungan : {}
-    hasil visit (VOC) ctb : {}
-    keterangan lain-lain : {}
-    foto kunjungan : {}
-    """.format(
-        data_recap["nip"],
-        data_recap["date"],
-        data_recap["voc"],
-        data_recap["other"],
-        data_recap["photo"]
-    )
+    recap_data = "----- rekap data input -----" \
+                 "\nnomor internet pelanggan : {}" \
+                 "\ntanggal kunjungan : {}" \
+                 "\nhasil visit (VOC) ctb : {}" \
+                 "\nketerangan lain-lain : {}" \
+                 "\nfoto kunjungan : {}".format(data_recap["nip"], data_recap["date"], data_recap["voc"],
+                                                data_recap["other"], data_recap["photo"]                                              )
     msg = "Apakah data yang anda isi sudah benar ? \n{}".format(recap_data)
-    keyboard = [[InlineKeyboardButton("salah", callback_data="salah"),
+    keyboard = [[InlineKeyboardButton("salah (input ulang)", callback_data="salah"),
                  InlineKeyboardButton("benar", callback_data="benar")]]
     keyboard_type_visit = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(
@@ -180,16 +192,19 @@ def confirm_handler(update, context):
 
 def confirm_callback(update, context):
     query = update.callback_query
+    msg_from_bot = update.callback_query.message.text
     state = query.data
+    query.edit_message_text(msg_from_bot + "\nKonfirmasi : %s" % query.data)
     if state == "benar":
         context.bot.send_message(
-            chat_id=update.callback_query.from_user.id,
-            text="Data visit berhasil disimpan",
-            reply_markup=ReplyMarkup()
+            chat_id=update.effective_chat.id,
+            text="Data visit berhasil disimpan" +
+                 "\n--- Sesi input data berakhir tekan /start untuk memulai input data kembali ---",
         )
         return ConversationHandler.END
     elif state == "salah":
         start(update, context)
+        return INPUT_CUST
 
 
 def other_handler(update, context):
@@ -198,7 +213,7 @@ def other_handler(update, context):
                  InlineKeyboardButton("ya", callback_data="ya")]]
     keyboard_type_visit = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(
-        chat_id=update.callback_query.from_user.id,
+        chat_id=update.effective_chat.id,
         text=msg,
         reply_markup=keyboard_type_visit
     )
@@ -219,14 +234,13 @@ def confirm_other_callback(update, context):
 def add_other_handler(update, context):
     msg = "Masukkan keterangan lain-lain: "
     context.bot.send_message(
-        chat_id=update.callback_query.from_user.id,
+        chat_id=update.effective_chat.id,
         text=msg
     )
 
 
 def add_other_callback(update, context):
     other_desc = update.message.text
-    update.message.reply_text(text="Keterangan lain-lain : {}".format(other_desc))
     data_recap['other'] = other_desc
     confirm_handler(update, context)
     return CONFIRM
@@ -242,8 +256,7 @@ if __name__ == "__main__":
             states={
                 INPUT_CUST: [MessageHandler(Filters.regex(r'\d+$'), cust_callback)],
                 DATE_VS: [CallbackQueryHandler(date_callback)],
-                PHOTO_VS: [MessageHandler(Filters.photo, photo_handler),
-                           CommandHandler('photo_input', photo_callback)],
+                PHOTO_VS: [MessageHandler(Filters.photo, photo_callback)],
                 STATE_VS: [CallbackQueryHandler(state_callback)],
                 CONTACT_VS: [CallbackQueryHandler(contact_callback)],
                 NOT_CONTACT_VS: [CallbackQueryHandler(not_contact_callback)],
@@ -252,7 +265,8 @@ if __name__ == "__main__":
                 CONFIRM: [CallbackQueryHandler(confirm_callback)]
             },
             fallbacks=[CommandHandler('cancel', cancel_callback),
-                       MessageHandler(Filters.all, fallback_handler)]
+                       MessageHandler(Filters.all, fallback_handler)],
+            allow_reentry=True
         )
         up.dispatcher.add_handler(conv_handler)
         up.dispatcher.add_error_handler(fallback_handler)
